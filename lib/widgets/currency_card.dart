e48801352520d5d10f915720e6bf95d2843b5416
04wakeup <42279557+04wakeup@users.noreply.github.com>
2023-03-17 19:56:38 -0700

    Translate to Class

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -5,6 +5,7 @@ class CurrencyCard extends StatelessWidget {
   final IconData icon;
   final bool isInverted;
   final blackColor = const Color(0xFF1F2123);
+  final int v_offSet;
 
   const CurrencyCard({
     super.key,
@@ -13,60 +14,67 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.isInverted,
+    required this.v_offSet,
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    color: isInverted ? blackColor : Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
+    return Transform.translate(
+      offset: v_offSet == 1
+          ? const Offset(0, 0)
+          : v_offSet == 2
+              ? const Offset(20, 20)
+              : const Offset(40, 40),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: isInverted ? blackColor : Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? blackColor : Colors.white,
-                        fontSize: 20,
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? blackColor : Colors.white,
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? blackColor
-                            : Colors.white.withOpacity(0.8),
-                        fontSize: 20,
+                      const SizedBox(
+                        width: 15,
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? blackColor
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
                 scale: 2,
                 child: Transform.translate(
                   offset: const Offset(
@@ -78,8 +86,10 @@ class CurrencyCard extends StatelessWidget {
                     color: isInverted ? blackColor : Colors.white,
                     size: 50,
                   ),
-                ))
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
